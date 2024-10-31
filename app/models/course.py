39fa89c9075ed101c365d757d991e24dda13a500
Rumeet Goradia <rumeet.goradia@gmail.com>
2024-10-30 22:30:02 -0400

    Clean up and introduce middleware and pagination (#2)
    * Clean up and introduce middleware and pagination
    
    * Add .gitignore
    
    * Rename
    
    * Clean up

--- app/models/course.py
+++ app/models/course.py
@@ -1,36 +1,31 @@
-from __future__ import annotations
-
-from typing import Optional
-
 from pydantic import BaseModel
 
 
-class CourseSection(BaseModel):
-    course_id: Optional[int] = None
-    course_name: Optional[str] = None
-    uuid: Optional[str] = None
-    p_uni: Optional[str] = None
-    created_at: Optional[str] = None
-    course_code: Optional[str] = None
-    course_id: Optional[str] = None
-    course_no: Optional[str] = None
-    section: Optional[str] = None
-    course_year: Optional[str] = None
-    semester: Optional[str] = None
+class Course(BaseModel):
+    course_id: str
+    course_name: str
+    call_num: int
+    professor: str
+    credits: int
+    is_prereq: bool
+    has_prereq: bool
+    is_core: bool
+    semester: str
+    year: int
 
     class Config:
-        json_schema_extra = {
+        orm_mode = True
+        schema_extra = {
             "example": {
-                "course_id": 204283,
-                "course_name": "COMSW4153_001_2024_3 - Cloud Computing",
-                "uuid": "3jHCxUV0ck9Z8TF1sZeI8WTx47olDGkX1YPL3USM",
-                "p_uni": "dff9",
-                "created_at": "2024-04-05T00:58:50Z",
-                "course_code": "COMSW4153_001_2024_3 - Cloud Computing",
-                "sis_course_id": "COMSW4153_001_2024_3",
-                "course_no": "COMSW4153",
-                "section": "001",
-                "course_year": "2024",
-                "semester": "3"
+                "course_id": "COMS4153W",
+                "course_name": "Cloud Computing",
+                "call_num": 12345,
+                "professor": "dff9",
+                "credits": 3,
+                "is_prereq": False,
+                "has_prereq": True,
+                "is_core": False,
+                "semester": "FA",
+                "year": 2024
             }
         }
--- app/services/service_factory.py
+++ app/services/service_factory.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from framework.services.service_factory import BaseServiceFactory
 from framework.services.data_access.MySQLRDBDataService import MySQLRDBDataService
 
-# TODO -- Implement this class
 class ServiceFactory(BaseServiceFactory):
 
     def __init__(self):
@@ -14,19 +13,16 @@ class ServiceFactory(BaseServiceFactory):
         load_dotenv()
 
         if service_name == 'CourseResource':
-            # Dynamic import to avoid circular dependency
             import app.resources.course_resource as course_resource
             result = course_resource.CourseResource(config=None)
 
         elif service_name == 'CourseResourceDataService':
-            # Populate the database information given the environment variable
             db_user = str(os.getenv("DB_USER"))
             db_password = str(os.getenv("DB_PASSWORD"))
-            db_host = str(os.getenv("DB_HOST"))  # Populate this with the URL of the DB
-            db_port = int(os.getenv("DB_PORT", 3307))  # Default to 3307 if not set
-            
-            context = dict(user=db_user, password=db_password,
-                           host=db_host, port=db_port)
+            db_host = str(os.getenv("DB_HOST"))
+            db_port = int(os.getenv("DB_PORT", 3307))
+
+            context = dict(user=db_user, password=db_password, host=db_host, port=db_port)
             data_service = MySQLRDBDataService(context=context)
             result = data_service
 
