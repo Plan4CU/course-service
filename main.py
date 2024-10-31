663622dcb150dc8dfb3e83699172156185b8c29b
Rumeet Goradia <rumeet.goradia@gmail.com>
2024-10-30 21:28:55 -0400

    Clean up

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import json
-from fastapi import FastAPI
-import uvicorn
-import os
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware import Middleware
-from app.routers import course_router
-from app.middleware.logging_middleware import LoggingMiddleware
 
-where_am_i = os.environ.get("WHEREAMI", None)
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.middleware.logging_middleware import LoggingMiddleware
+from app.routers import course_router
 
 middleware = [
     Middleware(CORSMiddleware, allow_origins=['*']),
@@ -16,6 +15,7 @@ middleware = [
 
 app = FastAPI(middleware=middleware)
 
+
 def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
@@ -23,15 +23,9 @@ def custom_openapi():
         app.openapi_schema = json.load(f)
         return app.openapi_schema
 
-app.openapi = custom_openapi
-app.include_router(courses_router.router)
 
-@app.get('/')
-def hello_world():
-    global where_am_i
-    if where_am_i is None:
-        where_am_i = "NOT IN DOCKER"
-    return f"Hello, from {where_am_i}! I changed and I'm microservice 3."
+app.openapi = custom_openapi
+app.include_router(course_router.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
